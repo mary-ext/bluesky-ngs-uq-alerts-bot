4937afd565e23ff4e2605946816c155b0d4052be
Mary <git@mary.my.id>
2025-03-01 10:29:38 +0700

    refactor: more robust

--- src/mod.ts
+++ src/mod.ts
@@ -6,105 +6,115 @@ import { scrape } from './urgent.ts';
 
 import configs from '../config.local.ts';
 
-const MINUTE = 60_000;
-
 await Deno.mkdir('./data', { recursive: true });
 
 for (const { id, scrapeUrl, mappings, account, buildPosts } of configs) {
-	console.log(`configuring ${id}`);
+	const run = async () => {
+		console.log(`[${id}]: scraping the page`);
 
-	Deno.cron(
-		`alert-${id}`,
-		'16,46 * * * *',
-		// 16 -> 18, 20, 22, 25
-		// 46 -> 48, 50, 52, 55
-		{ backoffSchedule: [MINUTE * 2, MINUTE * 2, MINUTE * 2, MINUTE * 3] },
-		async () => {
-			console.log(`[${id}]: scraping the page`);
+		const response = await fetch(scrapeUrl, {
+			signal: AbortSignal.timeout(30_000),
+			headers: {
+				'user-agent': 'codeberg:mary-ext/bluesky-ngs-uq-alerts',
+			},
+		});
 
-			const response = await fetch(scrapeUrl, {
-				headers: {
-					'user-agent': 'codeberg:mary-ext/bluesky-ngs-uq-alerts',
-				},
-			});
+		if (!response.ok) {
+			throw new Error(`unexpected response, got http ${response.status}`);
+		}
 
-			if (!response.ok) {
-				throw new Error(`unexpected response, got http ${response.status}`);
-			}
+		const source = await response.text();
+		const events = scrape(source, mappings);
 
-			const source = await response.text();
-			const events = scrape(source, mappings);
+		if (events.length === 0) {
+			throw new Error(`empty response`);
+		}
 
-			if (events.length === 0) {
-				throw new Error(`empty response`);
-			}
+		console.log(`[${id}]: got ${events.length} events`);
 
-			console.log(`[${id}]: got ${events.length} events`);
+		const posts = buildPosts(events);
+		console.log(`[${id}]: got ${posts.length} posts`);
 
-			const sessionFileUrl = `./data/${id}.session.json`;
+		if (posts.length === 0) {
+			console.log(`[${id}]: skipping`);
+			return;
+		}
 
-			const auth = new CredentialManager({
-				service: account.service,
-				onSessionUpdate(session) {
-					Deno.writeTextFileSync(sessionFileUrl, JSON.stringify(session, null, '\t'));
-				},
-				onExpired() {
-					try {
-						Deno.removeSync(sessionFileUrl);
-					} catch (err) {
-						if (err instanceof Deno.errors.NotFound) {
-							return;
-						}
-
-						throw err;
+		const sessionFileUrl = `./data/${id}.session.json`;
+		const auth = new CredentialManager({
+			service: account.service,
+			onSessionUpdate(session) {
+				Deno.writeTextFileSync(sessionFileUrl, JSON.stringify(session, null, '\t'));
+			},
+			onExpired() {
+				try {
+					Deno.removeSync(sessionFileUrl);
+				} catch (err) {
+					if (err instanceof Deno.errors.NotFound) {
+						return;
 					}
-				},
-			});
 
-			const rpc = new XRPC({ handler: auth });
-
-			try {
-				const rawSession = Deno.readTextFileSync(sessionFileUrl);
-				const session = JSON.parse(rawSession);
-
-				if (account.identifier !== session.handle && account.identifier !== session.did) {
-					throw new Deno.errors.InvalidData();
+					throw err;
 				}
+			},
+		});
 
-				console.log(`[${id}]: resuming session`);
+		const rpc = new XRPC({ handler: auth });
 
-				await auth.resume(session);
-			} catch (err) {
-				if (err instanceof Deno.errors.NotFound) {
-					console.log(`[${id}]: no session file found, creating new session`);
-				} else if (err instanceof Deno.errors.InvalidData) {
-					console.log(`[${id}]: session file is invalid, creating new session`);
-				} else if (err instanceof XRPCError && err.kind === 'ExpiredToken') {
-					console.log(`[${id}]: session expired, creating new session`);
-				} else {
-					console.error(err);
-					console.log(`[${id}]: skipping`);
+		try {
+			const rawSession = Deno.readTextFileSync(sessionFileUrl);
+			const session = JSON.parse(rawSession);
 
-					return;
-				}
-
-				await auth.login({ identifier: account.identifier, password: account.password });
+			if (account.identifier !== session.handle && account.identifier !== session.did) {
+				throw new Deno.errors.InvalidData();
 			}
 
-			console.log(`[${id}]: signed in as @${auth.session!.handle}`);
+			console.log(`[${id}]: resuming session`);
 
-			const posts = buildPosts(events);
-			console.log(`[${id}]: got ${posts.length} posts`);
+			await auth.resume(session);
+		} catch (err) {
+			if (err instanceof Deno.errors.NotFound) {
+				console.log(`[${id}]: no session file found, creating new session`);
+			} else if (err instanceof Deno.errors.InvalidData) {
+				console.log(`[${id}]: session file is invalid, creating new session`);
+			} else if (err instanceof XRPCError && err.kind === 'ExpiredToken') {
+				console.log(`[${id}]: session expired, creating new session`);
+			} else {
+				throw err;
+			}
 
-			if (posts.length === 0) {
+			await auth.login({ identifier: account.identifier, password: account.password });
+		}
+
+		console.log(`[${id}]: signed in as @${auth.session!.handle}`);
+
+		await publishThread(rpc, { author: auth.session!.did, posts: posts });
+		console.log(`[${id}]: posts published`);
+	};
+
+	{
+		const schedule: Deno.CronSchedule = {
+			minute: { exact: [16, 18, 20, 22, 25, 46, 48, 50, 52, 55] },
+		};
+
+		let lastRunSuccessful = false;
+
+		console.log(`[${id}]: scheduling`);
+		Deno.cron(id, schedule, async () => {
+			const currentMinute = new Date().getMinutes();
+			const shouldRun = !lastRunSuccessful || currentMinute === 16 || currentMinute === 46;
+
+			if (!shouldRun) {
 				console.log(`[${id}]: skipping`);
 				return;
 			}
 
-			await publishThread(rpc, { author: auth.session!.did, posts: posts });
-			console.log(`[${id}]: posts published`);
-		},
-	);
+			lastRunSuccessful = false;
+
+			await run();
+			lastRunSuccessful = true;
+		});
+	}
 }
 
 console.log(`running`);

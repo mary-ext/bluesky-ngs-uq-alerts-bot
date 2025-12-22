d92f470f0f1e58ea8f0d54bb476672a099c80560
Mary <git@mary.my.id>
2025-12-22 08:11:47 +0700

    chore: lint

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ export default {
 
 	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
 	// [[triggers]] configuration.
-	async scheduled(event, env, ctx): Promise<void> {
+	async scheduled(event, _env, _ctx): Promise<void> {
 		// A Cron Trigger can make requests to other endpoints on the Internet,
 		// publish to a Queue, query a D1 Database, and much more.
 		//
